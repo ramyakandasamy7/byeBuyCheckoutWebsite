77b0689c6d6a880246baceb7e3338e336a5e69e6
Jed Villanueva <jed.villanueva86@gmail.com>
2020-04-11 22:42:08 +0000

    Configured server with Apache and SSL

--- managerapi/managerapi.js
+++ managerapi/managerapi.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var app = express();
-var port = 3000;
+var express    = require('express');
+var app        = express();
+var https      = require('https');
+var fs         = require('fs');
+var port       = 3000;
 var bodyParser = require('body-parser');
-var cors = require('cors');
+var cors       = require('cors');
   
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//app.use(function(req, res) {
-//  res.status(404).send({url: req.originalUrl + ' not found'})
-//});
 
 var routes = require('./api/routes/managerApiRoutes'); //importing route
 routes(app); //register the route
 
-app.listen(port);
+https.createServer({
+	key: fs.readFileSync('certs/key.pem'),
+	cert: fs.readFileSync('certs/cert.pem')
+},app).listen(port);
 
-console.log('ByeBuyCheckout Manager RESTful API server started on: ' + port);
+console.log(Date.now() + ' -- ByeBuyCheckout Manager RESTful API server started on: ' + port);
 
